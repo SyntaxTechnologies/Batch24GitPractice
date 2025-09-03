ca8e0037a8af6972ede5ac13db277c4899cd394e
SyntaxTechnologies <moazzam@syntaxtechs.com>
2025-09-02 21:29:45 -0400

    2nd commit

--- src/test/java/class01/test.java
+++ src/test/java/class01/test.java
@@ -3,5 +3,11 @@ package class01;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello World");
+        System.out.println("hello World");
+        System.out.println("hello World"); System.out.println("hello World");
+
+
+
+
     }
 }
---
+++ src/test/java/class01/test2.java
@@ -0,0 +1,8 @@
+package class01;
+
+public class test2 {
+    String a;
+    String B;
+    String C;
+
+}
